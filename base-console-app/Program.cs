f98f57b1c73526eab55dfec05bb769bda00218aa
Kumar Priyank <kpriyank88@gmail.com>
2024-05-06 16:35:35 +0000

    commit

--- base-console-app/Program.cs
+++ base-console-app/Program.cs
@@ -1,107 +1,134 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Identity.Client;
-using Microsoft.Graph;
-using Microsoft.Extensions.Configuration;
+using System.Windows.Forms;
+using PortableConfiguration;
+using llc = LowLevelControls;
 
-namespace ConsoleGraphTest
+namespace PrintScreen
 {
     class Program
     {
-        private static GraphServiceClient _graphServiceClient;
-        private static HttpClient _httpClient;
+        static llc.KeyboardHook kbdHook = new llc.KeyboardHook();
+        static Config config = new Config(Path.Combine(AssemblyDirectory, "config"));
+        static volatile bool loop = true;
+        static bool prscDown = false;
+        static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
 
         static void Main(string[] args)
         {
-            // Load appsettings.json
-            var config = LoadAppSettings();
-            if (null == config)
+            Console.WriteLine("PrintScreen (made by j3soon)");
+            Console.WriteLine("1. Press PrintScreen to save the entire screen.");
+            Console.WriteLine("2. Press Alt+PrintScreen to save the current window.");
+            Console.WriteLine("3. Press Ctrl+C to exit.");
+            config.DefaultConfigEvent += () => { config["dir"] = @"%UserProfile%\Desktop\Screenshots\"; };
+            config.Load();
+            config.Save();
+            Console.WriteLine("4. The captured screens will be saved in: " + config["dir"]);
+            kbdHook.KeyDownEvent += kbdHook_KeyDownEvent;
+            kbdHook.KeyUpEvent += kbdHook_KeyUpEvent;
+            kbdHook.InstallGlobalHook();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            while (loop)
             {
-                Console.WriteLine("Missing or invalid appsettings.json file. Please see README.md for configuration instructions.");
-                return;
+                Application.DoEvents();
+                Thread.Sleep(1);
             }
+            kbdHook.UninstallGlobalHook();
+            config.Save();
+        }
 
-            //Query using Graph SDK (preferred when possible)
-            GraphServiceClient graphClient = GetAuthenticatedGraphClient(config);
-            List<QueryOption> options = new List<QueryOption>
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            loop = false;
+        }
+
+        private static bool kbdHook_KeyDownEvent(llc.KeyboardHook sender, uint vkCode, bool injected)
+        {
+            if (vkCode == (uint)Keys.PrintScreen && !prscDown)
             {
-                new QueryOption("$top", "1")
+                if (llc.Keyboard.IsKeyDown((int)Keys.Menu))
+                    Console.WriteLine("Saved Current Window - " + PrintWindow());
+                else
+                    Console.WriteLine("Saved Current Screen - " + PrintScreen());
+                prscDown = true;
+                return true;
+            }
+            return false;
+        }
+
+        private static bool kbdHook_KeyUpEvent(llc.KeyboardHook sender, uint vkCode, bool injected)
+        {
+            if (vkCode == (uint)Keys.PrintScreen)
+                prscDown = false;
+            return false;
+        }
+
+        static String GetScreenshotName()
+        {
+            String path = Environment.ExpandEnvironmentVariables(config["dir"]);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            path = Path.Combine(path, DateTime.Now.ToString("yyyyMMdd_HH-mm-ss"));
+            int num = 0;
+            for (; File.Exists(path + (num == 0 ? "" : ("_" + num)) + ".png"); num++) ;
+            return path + (num == 0 ? "" : ("_" + num)) + ".png";
+        }
+
+        static String PrintScreen()
+        {
+            llc.Natives.RECT rect = llc.Screen.GetScaledVirtualScreenRect();
+            Rectangle bounds = new Rectangle
+            {
+                X = rect.left,
+                Y = rect.top,
+                Width = rect.right - rect.left,
+                Height = rect.bottom - rect.top
             };
-
-            var graphResult = graphClient.Users.Request(options).GetAsync().Result;
-            Console.WriteLine("Graph SDK Result");
-            Console.WriteLine(graphResult[0].DisplayName);
-
-            //Direct query using HTTPClient (for beta endpoint calls or not available in Graph SDK)
-            HttpClient httpClient = GetAuthenticatedHTTPClient(config);
-            Uri Uri = new Uri("https://graph.microsoft.com/v1.0/users?$top=1");
-            var httpResult = httpClient.GetStringAsync(Uri).Result;
-
-            Console.WriteLine("HTTP Result");
-            Console.WriteLine(httpResult);
+            Bitmap bmp = new Bitmap(bounds.Width, bounds.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+                g.CopyFromScreen(bounds.Location, Point.Empty, bounds.Size);
+            // Since the program is single-threaded, race conditions won't happen
+            // For multi-threaded program, use the following with try-catch can guarantee that no file is overwritten:
+            // File.Open(path, FileMode.CreateNew)
+            String name = GetScreenshotName();
+            bmp.Save(name, ImageFormat.Png);
+            return name;
         }
 
-        private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
+        static String PrintWindow()
         {
-            var authenticationProvider = CreateAuthorizationProvider(config);
-            _graphServiceClient = new GraphServiceClient(authenticationProvider);
-            return _graphServiceClient;
-        }
-
-        private static HttpClient GetAuthenticatedHTTPClient(IConfigurationRoot config)
-        {
-            var authenticationProvider = CreateAuthorizationProvider(config);
-            _httpClient = new HttpClient(new AuthHandler(authenticationProvider, new HttpClientHandler()));
-            return _httpClient;
-        }
-
-        private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config)
-        {
-            var clientId = config["applicationId"];
-            var clientSecret = config["applicationSecret"];
-            var redirectUri = config["redirectUri"];
-            var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
-
-            //this specific scope means that application will default to what is defined in the application registration rather than using dynamic scopes
-            List<string> scopes = new List<string>();
-            scopes.Add("https://graph.microsoft.com/.default");
-
-            var cca = ConfidentialClientApplicationBuilder.Create(clientId)
-                                                    .WithAuthority(authority)
-                                                    .WithRedirectUri(redirectUri)
-                                                    .WithClientSecret(clientSecret)
-                                                    .Build();
-            return new MsalAuthenticationProvider(cca, scopes.ToArray());
-        }
-
-        private static IConfigurationRoot LoadAppSettings()
-        {
-            try
+            llc.Natives.RECT rect = llc.Window.GetWindowBounds(llc.Window.GetForegroundWindow());
+            Rectangle bounds = new Rectangle
             {
-                var config = new ConfigurationBuilder()
-                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true)
-                .Build();
-
-                // Validate required settings
-                if (string.IsNullOrEmpty(config["applicationId"]) ||
-                    string.IsNullOrEmpty(config["applicationSecret"]) ||
-                    string.IsNullOrEmpty(config["redirectUri"]) ||
-                    string.IsNullOrEmpty(config["tenantId"]) ||
-                    string.IsNullOrEmpty(config["domain"]))
-                {
-                    return null;
-                }
-
-                return config;
-            }
-            catch (System.IO.FileNotFoundException)
-            {
-                return null;
-            }
+                X = rect.left,
+                Y = rect.top,
+                Width = rect.right - rect.left,
+                Height = rect.bottom - rect.top
+            };
+            Bitmap bmp = new Bitmap(bounds.Width, bounds.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+                g.CopyFromScreen(bounds.Location, Point.Empty, bounds.Size);
+            String name = GetScreenshotName();
+            bmp.Save(name, ImageFormat.Png);
+            return name;
         }
     }
 }
